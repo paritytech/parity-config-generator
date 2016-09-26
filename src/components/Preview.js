3086ef53c9abea9508aab3d0f3019a7996828760
Tomusdrw <tomusdrw@gmail.com>
2016-09-26 11:07:28 +0200

    Saving config file

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,35 +1,57 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import './Preview.css';
 
 import data from '../data.json';
 // TODO [ToDr] move to some common?
 import {fillDescription} from './Editor';
 
-function Preview({settings, defaults}) {
-  return (
-    <div className="mdl-card mdl-shadow--2dp preview-card">
-      <div className="mdl-card__title">
-        <div className="preview-title mdl-card__title-text">
-          .parity/config.toml
+class Preview extends Component {
+  static propTypes = {
+    settings: PropTypes.object.isRequired,
+    defaults: PropTypes.object.isRequired
+  };
+
+  generateConfig = () => {
+    const {settings, defaults} = this.props;
+    const data = toToml(settings, defaults);
+    const filename = 'config.toml';
+    const blob = new Blob([data], {type: 'text/toml'});
+    if (window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveBlob(blob, filename);
+    } else {
+      var elem = window.document.createElement('a');
+      elem.href = window.URL.createObjectURL(blob);
+      elem.download = filename;        
+      document.body.appendChild(elem);
+      elem.click();        
+      document.body.removeChild(elem);
+    }
+  }
+
+  render() {
+    const {settings, defaults} = this.props;
+    return (
+      <div className="mdl-card mdl-shadow--2dp preview-card">
+        <div className="mdl-card__title">
+          <div className="preview-title mdl-card__title-text">
+            .parity/config.toml
+          </div>
+        </div>
+        <div className="mdl-card__actions mdl-card--border">
+          <textarea className="preview-editor" readOnly value={ toToml(settings, defaults) }></textarea>
+        </div>
+        <div className="mdl-card__menu">
+          <button
+            className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-efect"
+            onClick={this.generateConfig}>
+            <i className="material-icons" id="download">cloud download</i>
+            <span className="mdl-tooltip" htmlFor="download">Download Config File</span>
+          </button>
         </div>
       </div>
-      <div className="mdl-card__actions mdl-card--border">
-        <textarea className="preview-editor" readOnly value={ toToml(settings, defaults) }></textarea>
-      </div>
-      <div className="mdl-card__menu">
-        <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-efect">
-          <i className="material-icons" id="download">cloud download</i>
-          <span className="mdl-tooltip" htmlFor="download">Download Config File</span>
-        </button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
-Preview.propTypes = {
-  settings: PropTypes.object.isRequired,
-  defaults: PropTypes.object.isRequired
-};
 
 function toToml(settings, defaults) {
   const toml = Object.keys(settings).reduce((acc, section) => {
